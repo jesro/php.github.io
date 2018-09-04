1b5703476b652f88c86c5c90628430685e0b5752
jes <samueljesro@gmail.com>
2018-09-04 13:14:41 +0530

    Create config.js

---
+++ project/libs/config.js
@@ -0,0 +1,19 @@
+require.config({
+	baseUrl:'libs',
+	paths:{
+		'jquery':[
+		'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min',
+		'jquery-3.3.1.min'
+		],
+		'knockoutjs':[
+		'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
+		'knockout-3.4.2'
+		],
+		'product':'../modules/product'
+	},
+	shim:{
+		'jquery':{
+			exports:'$'
+		}
+	}
+});
